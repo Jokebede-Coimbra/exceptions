da86f753df5bdda933d57db9f0b9b7bc703fb86b
Coimbra <adsjkbd@gmail.com>
2022-07-16 13:25:56 -0300

    Good solution

---
+++ exceptions/DomainException.java
@@ -0,0 +1,9 @@
+package tratamento.excecoes.model.exceptions;
+
+public class DomainException extends RuntimeException {
+
+    public DomainException(String msg) {
+        super(msg);
+    }
+    
+}
